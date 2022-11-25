3cf4a992b93734d829603eed09727080db12ede0
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-25 15:46:35 +0100

    Creazione tag form

--- index.php
+++ index.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>PHP Badwords</title>
 </head>
 <body>
+    
+
+    <div class="container">
+        <form>
+          <div class="my-3">
+            <label for="exampleInputEmail1" class="form-label">Paragrafo</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Parola censurata</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
     
 </body>
 </html>
